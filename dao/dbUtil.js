808f245b9f8f8fa8f3e488f3cc9cffbe5f2b928c
zyy <530357897@qq.com>
2019-04-15 16:26:43 +0800

    修改host

--- dao/dbUtil.js
+++ dao/dbUtil.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 function createConnection() {
     var connection = mysql.createConnection({
-        host: '127.0.0.1',
+        host: '192.168.10.26',
         port: '3306',
         user: 'root',
         password: 'aa123456',
